ef4a9ed37762f76b3405742d32f767f72a9a2f7f
Velavan Sundharamoorthy <velavan.sundharamoorth@kalvium.community>
2023-08-17 17:48:10 +0530

    Mission Accomplished

--- src/app.js
+++ src/app.js
@@ -47,18 +47,42 @@ function renderPatty() {
 
 function renderCheese() {
   //Trial 1 - Change the visibility of cheese based on state by manipulating the DOM
+  let cheese = document.querySelector("#cheese");
+  if (state.Cheese) {
+    cheese.style.display = "inherit";
+  } else {
+    cheese.style.display = "none";
+  }
 }
 
 function renderTomatoes() {
   //Trial 1 - Change the visibility of Tomatoes based on state by manipulating the DOM
+  let tomatoes = document.querySelector("#tomatoes");
+  if (state.Tomatoes) {
+    tomatoes.style.display = "inherit";
+  } else {
+    tomatoes.style.display = "none";
+  }
 }
 
 function renderOnions() {
   //Trial 1 - Change the visibility of Onions based on state by manipulating the DOM
+  let onions = document.querySelector("#onions");
+  if (state.Onions) {
+    onions.style.display = "inherit";
+  } else {
+    onions.style.display = "none";
+  }
 }
 
 function renderLettuce() {
   //Trial 1 - Change the visibility of Lettuce based on state by manipulating the DOM
+  let lettuce = document.querySelector("#lettuce");
+  if (state.Lettuce) {
+    lettuce.style.display = "inherit";
+  } else {
+    lettuce.style.display = "none";
+  }
 }
 
 document.querySelector(".btn-patty").onclick = function () {
@@ -67,23 +91,143 @@ document.querySelector(".btn-patty").onclick = function () {
 };
 
 // Trial 2 - Setup event listener for the cheese button
-
+document.querySelector(".btn-cheese").onclick = function () {
+  state.Cheese = !state.Cheese;
+  renderAll();
+};
 
 // Trial 2 - Setup event listener for the tomatoes button
+document.querySelector(".btn-tomatoes").onclick = function () {
+  state.Tomatoes = !state.Tomatoes;
+  renderAll();
+};
 
 
 // Trial 2 - Setup event listener for the onion button
+document.querySelector(".btn-onions").onclick = function () {
+  state.Onions = !state.Onions;
+  renderAll();
+};
 
 
 // Trial 2 - Setup event listener for the lettuce button
-
+document.querySelector(".btn-lettuce").onclick = function () {
+  state.Lettuce = !state.Lettuce;
+  renderAll();
+};
 
 //Challenge 1 - Add/Remove the class active to the buttons based on state
+document.querySelector('.btn-patty').addEventListener('click',()=>{
+  if (!(state.Patty)){
+    document.querySelector('.btn-patty').classList.remove('active')
+  }
+  else{
+    document.querySelector('.btn-patty').classList.add('active')
+  }
+})
 
+document.querySelector('.btn-cheese').addEventListener('click',()=>{
+  if (!(state.Cheese)){
+    document.querySelector('.btn-cheese').classList.remove('active')
+  }
+  else{
+    document.querySelector('.btn-cheese').classList.add('active')
+  }
+})
+
+document.querySelector('.btn-tomatoes').addEventListener('click',()=>{
+  if (!(state.Tomatoes)){
+    document.querySelector('.btn-tomatoes').classList.remove('active')
+  }
+  else{
+    document.querySelector('.btn-tomatoes').classList.add('active')
+  }
+})
+
+document.querySelector('.btn-onions').addEventListener('click',()=>{
+  if (!(state.Onions)){
+    document.querySelector('.btn-onions').classList.remove('active')
+  }
+  else{
+    document.querySelector('.btn-onions').classList.add('active')
+  }
+})
+
+document.querySelector('.btn-lettuce').addEventListener('click',()=>{
+  if (!(state.Lettuce)){
+    document.querySelector('.btn-lettuce').classList.remove('active')
+  }
+  else{
+    document.querySelector('.btn-lettuce').classList.add('active')
+  }
+})
 
 //Challenge 2 - Render only the items selected in the ingredients board based on the state
+const list = document.querySelectorAll('.items');
+var total=170
 
+document.querySelector('.btn-patty').addEventListener('click',()=>{
+  if(!(state.Patty)){
+    list[0].style.display='none'
+    total = total-ingredients[list[0].textContent]
+  }
+  else{
+    list[0].style.display='block'
+    total = total+ingredients[list[0].textContent]
+  }
+})
+
+document.querySelector('.btn-cheese').addEventListener('click',()=>{
+  if(!(state.Cheese)){
+    list[1].style.display='none'
+    total = total-ingredients[list[1].textContent]
+  }
+  else{
+    list[1].style.display='block'
+    total = total+ingredients[list[1].textContent]
+  }
+})
+
+document.querySelector('.btn-tomatoes').addEventListener('click',()=>{
+  if(!(state.Tomatoes)){
+    list[2].style.display='none'
+    total = total-ingredients[list[2].textContent]
+  }
+  else{
+    list[2].style.display='block'
+    total = total+ingredients[list[2].textContent]
+  }
+})
+
+document.querySelector('.btn-onions').addEventListener('click',()=>{
+  if(!(state.Onions)){
+    list[3].style.display='none'
+    total = total-ingredients[list[3].textContent]
+  }
+  else{
+    list[3].style.display='block'
+    total = total+ingredients[list[3].textContent]
+  }
+})
+
+document.querySelector('.btn-lettuce').addEventListener('click',()=>{
+  if(!(state.Lettuce)){
+    list[4].style.display='none'
+    total = total-ingredients[list[4].textContent]
+  }
+  else{
+    list[4].style.display='block'
+    total = total+ingredients[list[4].textContent]
+  }
+})
 
 //Judgement 1
 //In the p element having price-details as the class, display the calculated
 //price based on ingredients
+
+const button = document.querySelectorAll('.button');
+for (let i=0;i<button.length;i++){
+  button[i].addEventListener('click',()=>{
+    document.querySelector('.price-details').innerText = 'INR'+total
+  })
+}
